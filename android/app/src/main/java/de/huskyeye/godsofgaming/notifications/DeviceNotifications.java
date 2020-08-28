8fd002b206c142ca567b352c0ab4f3d8d633d2f9
Joshua Steffensky <steffensky@cs.uni-saarland.de>
2020-08-28 13:30:53 +0200

    init stuff, no more motivation

---
+++ android/app/src/main/java/de/huskyeye/godsofgaming/notifications/DeviceNotifications.java
@@ -0,0 +1,11 @@
+package de.huskyeye.godsofgaming.notifications;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+
+public class DeviceNotifications {
+    public void deliverNotification(String title, String text, int icon, String onclickurl)
+    {
+
+    }
+}

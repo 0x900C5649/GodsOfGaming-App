3874c8d94d4e608c881e7f087edbe5aa88cf7682
Joshua Steffensky <steffensky@cs.uni-saarland.de>
2020-08-28 13:13:40 +0200

    init android

---
+++ android/app/src/main/java/de/huskyeye/godsofgaming/MainActivity.java
@@ -0,0 +1,14 @@
+package de.huskyeye.godsofgaming;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
